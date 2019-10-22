cec34bb8e269da2f604188ccf48f554ce467e4b7
Spagcat <orockshel@gmail.com>
2019-10-21 21:35:52 -0400

    mostly figured it out

--- index.js
+++ index.js
@@ -16,36 +16,79 @@ const fetchEverything = Promise.all([products, offerings, companies])
 .then(responses => {
     const [products, offerings, companies] = responses;
     
+    const allOfferings = getOfferings(products, offerings, companies);
+    console.log(allOfferings);
+
     products.forEach(product => {
         const newCard = document.createElement('div');
         const header = document.createElement('h1');
         const description = document.createElement('p');
         const price = document.createElement('p');
-        const list = document.createElement('ul');
 
         header.innerHTML = product.name;
         description.innerHTML = product.description;
         price.innerHTML = product.suggestedPrice;
 
-        let id = product.id;
-        let offeringsArr = [
-            companies: [],
-        ];
-        offerings.forEach(offering => {
-            if (offering.productId === id) {
-                offeringsArr.push({companies})
-            }
-        })
+        newCard.appendChild(header);
+        newCard.appendChild(description);
+        newCard.appendChild(price);
 
+        allOfferings.forEach(product => {
+            const list = document.createElement('ul');
+            product.offers.forEach(offer => {
+                let listItem = document.createElement('li')
+                listItem.innerHTML = `Offered by: ${offer.companyName} $${offer.price}`
+                list.appendChild(listItem);
+            });
+            newCard.appendChild(list);
+        });
+
+
+        
+
+
+        // let id = product.id;
+        // let offeringsArr = [
+        //     companies: [],
+        // ];
+        // offerings.filter(offering => {
+
+        // });
+        // console.log(offerings);
         app.appendChild(newCard);
-    })
+    });
+
 
 })
 
-// const getOfferings = (products, offerings, companies) => {
-//     offerings.forEach(offering => {
+const getOfferings = (products, offerings, companies) => {
+    // let obj = {
+    //     companies: [],
+    //     prices: [],
+    // };
 
-//     })
-// }
+    const mappedProducts = products.map(product => {
+        let currObj = {
+            product: product.id,
+            offers: offerings.filter(offer => {
+                return offer.productId === product.id
+            }),
+        }
+
+        return currObj
+        
+        // if(!obj[product.id]) {
+        //     obj[product.id] = product.id 
+
+    });
+    // console.log(mappedProducts);
+
+    mappedProducts.forEach(product => {
+        product.offers.forEach(offer => {
+            offer.companyName = companies.find(company => company.id === offer.companyId).name;
+        })
+    })
+    return mappedProducts
+}
 
 // console.log(fetchEverything);
